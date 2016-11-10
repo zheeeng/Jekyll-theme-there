4f85a75fed1db8f4f4cd2b7e4ebaac58e12fafcc
Zheeeng <hi@zheeeng.me>
2016-11-10 20:42:46 +0800

    Decrease the function nestings

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,46 @@
 window.onload = function () {
   var shims = {
+    forEach: null,
+    getClosestParentElementByClassName: null,
     addEvent: null,
     loopEventByFrame: null,
     cancelLoopEventByFrame: null
   }
+
+  ;(function (shims) {
+    shims.forEach = (function () {
+      if (!Array.prototype.forEach) {
+        return function (arrayLikeList, callback, thisArg) {
+          Array.prototype.forEach.call(arrayLikeList, callback, thisArg)
+        }
+      } else {
+        return function (arrayLikeList, callback, thisArg) {
+          if (arrayLikeList === null || arrayLikeList === void 0) throw new TypeError('parameter arrayLikeList is null or undefined')
+          if (typeof callback !== 'function') throw new TypeError('parameter callback is not a function')
+          var o = Object(arrayLikeList)
+          var len = o.length
+          var k = 0
+          while (k < len) {
+            if (k in o) callback.call(thisArg, o[k], k, o)
+            k++
+          }
+        }
+      }
+    })()
+  })(shims)
+
+  ;(function (shims) {
+    shims.getClosestParentElementByClassName = function ($el, className) {
+      while ($el) {
+        if ($el.classList.contains(className)) {
+          return $el
+        } else {
+          $el = $el.parentElement
+        }
+      }
+      return null
+    }
+  })(shims)
 
   ;(function (shims) {
     shims.addEvent = (function () {
@@ -54,103 +91,132 @@ window.onload = function () {
   // Pages initializing
   ;(function initTopicPage (shims) {
     // Import shims
+    var forEach = shims.forEach
     var addEvent = shims.addEvent
     var loopEventByFrame = shims.loopEventByFrame
     var cancelLoopEventByFrame = shims.cancelLoopEventByFrame
-    // For theme switching
+    // HTML element class hooks
     var classContainer = 'j-overview-topic-container'
-    var classHeading = 'j-overview-topic-heading'
+    var classItem = 'j-overview-topic-item'
     var classChangeAnchor = 'j-overview-topic-change'
-    var __headingSelected = 'overview_topic-heading__selected'
+    var __itemSelected = 'overview_topic-item__selected'
     // For navigation auto scrolling
-    // Variable scrollReservZoneWidth define the width of the reserve zone on where the mousemove event not take effect. The reserve zone locate in the beginning and the end of the scroll mapping zone. The scrollSpeedFactor control the speed of scrolling
+    // Variable scrollReservZoneWidth define the width of the reserve zone on where the mousemove event take no effect. The reserve zone locate in the beginning and the end of the scroll mapping zone. The scrollSpeedFactor control the speed of scrolling
     var classNavScrollZone = 'j-overview-topic-nav'
     var scrollReserveZoneWidth = 40
     var scrollSpeedFactor = 0.2
+    // Prepared stuffs
+    var bodyRect = document.body.getBoundingClientRect()
 
     function switchTheme ($el, theme) {
-      [].slice.call($el.classList).forEach(function (className) {
+      forEach($el.classList, function (className) {
         if (/^theme\-/.test(className)) $el.classList.remove(className)
       })
       $el.classList.add('theme-' + theme)
     }
 
-    function getClosestParentElementByClassName ($el, className) {
-      while ($el) {
-        if ($el.classList.contains(className)) {
-          return $el
-        } else {
-          $el = $el.parentElement
-        }
+    // Decoupled processors
+    function processContainer ($container, index, $containers) {
+      var ctx = {
+        $container: $container
       }
-      return null
+      var $navs = $container.getElementsByClassName(classNavScrollZone)
+      if ($navs) forEach($navs, processNavigation, ctx)
     }
 
-    var $topicContainers = document.getElementsByClassName(classContainer)
-    if ($topicContainers) {
-      [].forEach.call($topicContainers, function ($container) {
-        [].forEach.call($container.getElementsByClassName(classChangeAnchor), function ($anchor) {
-          addEvent($anchor, 'click', function (e) {
-            e.preventDefault()
-            switchTheme($container, $anchor.getAttribute('data-theme'))
-            ;[].forEach.call($container.getElementsByClassName(classHeading), function ($heading) {
-              $heading.classList.remove(__headingSelected)
-            })
-            getClosestParentElementByClassName($anchor, classHeading).classList.add(__headingSelected)
-          })
-        })
-      })
+    // process switch themes and assign the selected item to $selectedItem, it require passing the $container to be themed
+    function processNavigation ($nav) {
+      var ctx = {
+        $container: this.$container,
+        $nav: $nav,
+        $selectedItem: null
+      }
+
+      makeNavigationScrollable($nav)
+      var $items = $nav.getElementsByClassName(classItem)
+      if ($items) forEach($items, processItem, ctx)
     }
 
-    var $navs = document.getElementsByClassName(classNavScrollZone)
-    if ($navs) {
-      var bodyRect = document.body.getBoundingClientRect()
-      ;[].forEach.call($navs, function ($nav) {
-        var navRect = $nav.getBoundingClientRect()
-        var navClientWidth = $nav.clientWidth
-        var scrollFrameId
-        // mouseOverX = the relative position X of mouse in $nav.clientWidth
-        // mouseOverRateX = mouseOverX in percentage(exclude the reserve zone)
-        var mouseOverX, mouseOverRateX
-        // navScrollMaxDistance = $nav.scrollWdith - $nav.clientWidth in real time
-        // navScrollLeft = $nav.scrollLeft in real time
-        // navScrollLeftTo = map the variable mouseOverX in $nav.clientWidth (exclude the reserve zone, I employee the variable mouseOverRateX) to the distance X of navScrollMaxDistance in real time
-        // navScrollLeftDistance = navScrollLeftTo - navScrollLeft
-        var navScrollMaxDistance, navScrollLeft, navScrollLeftTo, navScrollLeftDistance
+    // Mouse moving on navigation zone scroll the topics into the navigation view zone
+    function makeNavigationScrollable ($nav) {
+      var navRect = $nav.getBoundingClientRect()
+      var navClientWidth = $nav.clientWidth
+      var scrollFrameId
+      // mouseOverX = the relative position X of mouse in $nav.clientWidth
+      // mouseOverRateX = mouseOverX in percentage(exclude the reserve zone)
+      var mouseOverX, mouseOverRateX
+      // navScrollMaxDistance = $nav.scrollWdith - $nav.clientWidth in real time
+      // navScrollLeft = $nav.scrollLeft in real time
+      // navScrollLeftTo = map the variable mouseOverX in $nav.clientWidth (exclude the reserve zone, I employee the variable mouseOverRateX) to the distance X of navScrollMaxDistance in real time
+      // navScrollLeftDistance = navScrollLeftTo - navScrollLeft
+      var navScrollMaxDistance, navScrollLeft, navScrollLeftTo, navScrollLeftDistance
 
-        addEvent($nav, 'mousemove', function (e) {
-          mouseOverX = e.pageX - (navRect.left - bodyRect.left)
-          mouseOverRateX = mouseOverX < scrollReserveZoneWidth ? 0
-            : mouseOverX > navClientWidth - scrollReserveZoneWidth ? 1
-            : (mouseOverX - scrollReserveZoneWidth) / (navClientWidth - scrollReserveZoneWidth * 2)
-          cancelLoopEventByFrame(scrollFrameId)
-          scrollFrameId = loopEventByFrame(function loop () {
-            if ($nav.scrollWidth <= navClientWidth) return
-            navScrollMaxDistance = $nav.scrollWidth - navClientWidth
-            navScrollLeft = $nav.scrollLeft
-            navScrollLeftTo = navScrollMaxDistance * mouseOverRateX
-            navScrollLeftDistance = navScrollLeftTo - navScrollLeft
-            if (navScrollLeftDistance > 0) {
-              // Positive distance means $nav need to scroll toward right
-              if (navScrollLeftTo * scrollSpeedFactor < navScrollLeftDistance) {
-                $nav.scrollLeft += navScrollLeftTo * scrollSpeedFactor
-                scrollFrameId = loopEventByFrame(loop)
-              } else {
-                $nav.scrollLeft += navScrollLeftDistance
-              }
-            } else if (navScrollLeftDistance < 0) {
-              navScrollLeftDistance = -navScrollLeftDistance
-              // Negative distance means $nav need to scroll toward left
-              if ((navScrollMaxDistance - navScrollLeftTo) * scrollSpeedFactor < navScrollLeftDistance) {
-                $nav.scrollLeft -= (navScrollMaxDistance - navScrollLeftTo) * scrollSpeedFactor
-                scrollFrameId = requestAnimationFrame(loop)
-              } else {
-                $nav.scrollLeft -= navScrollLeftDistance
-              }
+      // Mouse moving event trigger navigation scrolling
+      addEvent($nav, 'mousemove', function (e) {
+        mouseOverX = e.pageX - (navRect.left - bodyRect.left)
+        mouseOverRateX = mouseOverX < scrollReserveZoneWidth ? 0
+          : mouseOverX > navClientWidth - scrollReserveZoneWidth ? 1
+          : (mouseOverX - scrollReserveZoneWidth) / (navClientWidth - scrollReserveZoneWidth * 2)
+        cancelLoopEventByFrame(scrollFrameId)
+        scrollFrameId = loopEventByFrame(function loop () {
+          if ($nav.scrollWidth <= navClientWidth) return
+          navScrollMaxDistance = $nav.scrollWidth - navClientWidth
+          navScrollLeft = $nav.scrollLeft
+          navScrollLeftTo = navScrollMaxDistance * mouseOverRateX
+          navScrollLeftDistance = navScrollLeftTo - navScrollLeft
+          if (navScrollLeftDistance > 0) {
+            // Positive distance means $nav need to scroll toward right
+            if (navScrollLeftTo * scrollSpeedFactor < navScrollLeftDistance) {
+              $nav.scrollLeft += navScrollLeftTo * scrollSpeedFactor
+              scrollFrameId = loopEventByFrame(loop)
+            } else {
+              $nav.scrollLeft += navScrollLeftDistance
             }
-          })
+          } else if (navScrollLeftDistance < 0) {
+            navScrollLeftDistance = -navScrollLeftDistance
+            // Negative distance means $nav need to scroll toward left
+            if ((navScrollMaxDistance - navScrollLeftTo) * scrollSpeedFactor < navScrollLeftDistance) {
+              $nav.scrollLeft -= (navScrollMaxDistance - navScrollLeftTo) * scrollSpeedFactor
+              scrollFrameId = requestAnimationFrame(loop)
+            } else {
+              $nav.scrollLeft -= navScrollLeftDistance
+            }
+          }
         })
       })
+      // Mouse leaving navigation set right the selected item
+    }
+
+    // Make clicking on anchor switch the themes and assign the selected item to $selectedItem
+    function processItem ($item, index, $items) {
+      var ctx = {
+        $container: this.$container,
+        $nav: this.$nav,
+        $selectedItem: this.$selectedItem,
+        $item: $item
+      }
+
+      // Must the first anchor works on clicking
+      var $anchors = $item.getElementsByClassName(classChangeAnchor)
+      var $anchor = $anchors ? $anchors[0] : null
+
+      if ($anchor) {
+        addEvent($anchor, 'click', function (e) {
+          e.preventDefault()
+          switchTheme(ctx.$container, $anchor.getAttribute('data-theme'))
+          forEach($items, function ($item) {
+            $item.classList.remove(__itemSelected)
+          })
+          $item.classList.add(__itemSelected)
+          ctx.$selectedItem = $item
+        })
+      }
+    }
+
+    // Clicking topic change anchor switch the theme mounted on the topic container
+    var $containers = document.getElementsByClassName(classContainer)
+    if ($containers) {
+      forEach($containers, processContainer)
     }
   })(shims)
 }
